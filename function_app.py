3e8d209a91eeef00268497e58d3335b05a336ebd
Lesedi Kekana <theoriginalles@gmail.com>
2024-07-01 16:53:03 +0200

    confirmed that it allows multiple py files

--- function_app.py
+++ function_app.py
@@ -1,12 +1,13 @@
 import azure.functions as func
 import logging
+from file import hello_world
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="HttpExample")
 def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    hello_world()
     name = req.params.get('name')
     if not name:
         try:

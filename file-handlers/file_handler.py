49bf44d4853c2f735cf1a8b0fae6cf328edd30d3
Lesedi Kekana <theoriginalles@gmail.com>
2024-07-03 10:30:11 +0200

    created generic file handler with output validation

---
+++ file-handlers/file_handler.py
@@ -0,0 +1,90 @@
+from abc import ABC, abstractmethod
+from io import BufferedReader
+import json
+from typing import List
+from ..llm.llmstrategy import LLMStrategy
+from jsonschema import validate, ValidationError
+import traceback
+
+'''
+when parsing cloud certificate data, we need to check if the response contains the following:
+- name & surname
+- certification
+- qualification level (optional)
+- cloud provider
+- issue date
+- expiry date (optional)
+- certificate id
+'''
+
+'''
+json structure:
+{
+    "name": "John Doe",
+    "certification": "Cloud Practitioner",
+    "qualification_level": "Associate",
+    "cloud_provider": "AWS",
+    "issue_date": "2022-01-01",
+    "expiry_date": "2025-01-01",
+    "certificate_id": "123456"
+}
+'''
+
+'''
+json schema:
+{
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "certification": {"type": "string"},
+        "qualification_level": {"type": "string"},
+        "cloud_provider": {"type": "string"},
+        "issue_date": {"type": "string"},
+        "expiry_date": {"type": "string"},
+        "certificate_id": {"type": "string"}
+    },
+    "required": ["name", "certification", "cloud_provider", "issue_date", "certificate_id"]
+}
+'''
+
+JSON_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "certification": {"type": "string"},
+        "qualification_level": {"type": "string"},
+        "cloud_provider": {"type": "string"},
+        "issue_date": {"type": "string"},
+        "expiry_date": {"type": "string"},
+        "certificate_id": {"type": "string"}
+    },
+    "required": ["name", "certification", "cloud_provider", "issue_date", "certificate_id"]
+}
+
+class FileHandler(ABC):
+    def __init__(self, llm_strategy: LLMStrategy, max_retry: int = 3) -> None:
+        self.llm_strategy = llm_strategy
+        self.max_retry = max_retry
+
+    def process_file(self, file_buffer: BufferedReader) -> dict:
+        pass
+
+    def process_file_with_custom_prompt(self, file_buffer: BufferedReader, prompt: str) -> dict:
+        pass
+
+    def valid_response(self, response: str) -> bool:
+        try:
+            j = json.loads(response)
+            if "error" in j:
+                return False
+            validate(j, JSON_SCHEMA)
+        except json.JSONDecodeError:
+            return False
+        except ValidationError:
+            return False
+        except Exception as e:
+            print("Error validating response")
+            traceback.print_exc()
+            return False
+
+        return True
